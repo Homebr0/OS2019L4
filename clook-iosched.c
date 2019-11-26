908f7c8cf8086614b4a82d962109bb94873173a4
Jason Davila <jsndavila1@gmail.com>
2019-11-26 15:11:37 -0500

    Update clook-iosched.c

--- clook-iosched.c
+++ clook-iosched.c
@@ -1,12 +1,3 @@
-/*
- *   elevator clook
- *
- *   MODIFIED BY MARK OLESON & ALEXIS JEFFERSON
- *   FOR COP4610 COURSE, LAB 4
- *
- *   Only edited the clook_add_request and clook_dispatch functions
- *
- */
 #include <linux/blkdev.h>
 #include <linux/elevator.h>
 #include <linux/bio.h>
@@ -14,126 +5,160 @@
 #include <linux/slab.h>
 #include <linux/init.h>
 
-struct clook_data {
+int diskhead = -1;
+
+// STRUCT:  clook data with pos 
+struct clook_data
+{
 	struct list_head queue;
 };
 
-static void clook_merged_requests(struct request_queue *q, struct request *rq,
-				 struct request *next)
+// Merge Requests
+static void clook_merged_requests(struct request_queue *q, struct request *rq, struct request *next)
 {
+	// Delete request from list, then sort
 	list_del_init(&next->queuelist);
+	elv_dispatch_sort(q, next);
 }
 
-/* Edited this function to add the print statement, which prints out 
-   whether the data was being written (W) or read (R)  whenever the
-   dispatcher is called.  */
+// Dispatch first request in queue
+/* MADE CHANGES HERE */
 static int clook_dispatch(struct request_queue *q, int force)
 {
 	struct clook_data *nd = q->elevator->elevator_data;
-
-	if (!list_empty(&nd->queue)) {
-		struct request *rq;
-		rq = list_entry(nd->queue.next, struct request, queuelist);
+	struct request *rq;
+	char direction;
+	
+	rq = list_first_entry_or_null(&nd->queue, struct request, queuelist);
+	if (rq) {
+		// Delete request from list, then sort
 		list_del_init(&rq->queuelist);
 		elv_dispatch_sort(q, rq);
-
-		char readwrite = (rq_data_dir(rq) & REQ_WRITE) ? 'W' : 'R';
-		printk("[CLOOK] dsp %c %lu\n", readwrite, blk_rq_pos(rq));
-
+		
+		// Assign diskhead to position of rq
+		diskhead = blk_rq_pos(rq);
+		
+		// Check direction for read or write
+		if(rq_data_dir(rq) == READ)
+			direction = 'R';
+		else
+			direction = 'W';
+		printk("[CLOOK] dsp %c %lu\n", direction, blk_rq_pos(rq));
+		
 		return 1;
 	}
 	return 0;
 }
 
-/* Edited this function to organize the requests in the list by their 
-   physical location on the disk. Also added the print statement to print 
-   out whether the data was being written (W) or read (R) whenever a request
-   is added. */
+// Add request to queue
+/* MADE CHANGES HERE */
 static void clook_add_request(struct request_queue *q, struct request *rq)
 {
 	struct clook_data *nd = q->elevator->elevator_data;
 	struct list_head *cur = NULL;
-
-	/* This loop puts the request in the right order by comparing physical locations */
-	list_for_each(cur, &nd->queue) {
-		if(rq_end_sector(list_entry(cur, struct request, queuelist)) > rq_end_sector(rq)) {
-			break;
+	char direction;
+	
+	// Iterate through the request_queue elevator
+	list_for_each(cur, &nd->queue)
+	{
+		// Set cur to cur list_entry
+		struct request *c = list_entry(cur, struct request, queuelist);
+		
+		// If cur position is greater than diskhead...
+		if (blk_rq_pos(rq) > diskhead)
+		{
+			// If the cur position < diskhead OR rq position < cur position... break and add to cur position
+			if(blk_rq_pos(c) < diskhead || blk_rq_pos(rq) < blk_rq_pos(c))
+				break;
+			
+		} else {
+			// cur pos less than disk head
+			// If the cur position < diskhead AND rq position < cur position... break and add to cur position
+			if(blk_rq_pos(c) < diskhead && blk_rq_pos(rq) < blk_rq_pos(c))
+				break;
 		}
 	}
+	
+	// Add cur request to request queue
 	list_add_tail(&rq->queuelist, cur);
-
-	char readwrite = (rq_data_dir(rq) & REQ_WRITE) ? 'W' : 'R';
-	printk("[CLOOK] add %c %lu\n", readwrite, blk_rq_pos(rq));
+	
+	// Check direction for read or write
+	if(rq_data_dir(rq) == READ)
+		direction = 'R';
+	else
+		direction = 'W';
+	printk("[CLOOK] dsp %c %lu\n", direction, blk_rq_pos(rq));
 }
 
-static int clook_queue_empty(struct request_queue *q)
+// Get former request
+static struct request * clook_former_request(struct request_queue *q, struct request *rq)
 {
-	struct clook_data *nd = q->elevator->elevator_data;
+        struct clook_data *nd = q->elevator->elevator_data;
 
-	return list_empty(&nd->queue);
+        if (rq->queuelist.prev == &nd->queue)
+			return NULL;
+        
+		return list_prev_entry(rq, queuelist);
 }
 
-static struct request *
-clook_former_request(struct request_queue *q, struct request *rq)
-{
-	struct clook_data *nd = q->elevator->elevator_data;
-
-	if (rq->queuelist.prev == &nd->queue)
-		return NULL;
-	return list_entry(rq->queuelist.prev, struct request, queuelist);
-}
-
-static struct request *
-clook_latter_request(struct request_queue *q, struct request *rq)
-{
-	struct clook_data *nd = q->elevator->elevator_data;
-
-	if (rq->queuelist.next == &nd->queue)
-		return NULL;
-	return list_entry(rq->queuelist.next, struct request, queuelist);
-}
-
-static void *clook_init_queue(struct request_queue *q)
+// Initialize Queue
+static int clook_init_queue(struct request_queue *q, struct elevator_type *e)
 {
 	struct clook_data *nd;
-
+	struct elevator_queue *eq;
+	
+	eq = elevator_alloc(q, e);
+	if(!eq)
+		return -ENOMEM;
+	
 	nd = kmalloc_node(sizeof(*nd), GFP_KERNEL, q->node);
-	if (!nd)
-		return NULL;
+	if (!nd) {
+		kobject_put(&eq->kobj);
+		return -ENOMEM;
+	}
+	
+	eq->elevator_data = nd;
+	
 	INIT_LIST_HEAD(&nd->queue);
-	return nd;
+	
+	spin_lock_irq(q->queue_lock);
+	q->elevator = eq;
+	spin_unlock_irq(q->queue_lock);
+	
+	return 0;
 }
 
+// Exit Queue
 static void clook_exit_queue(struct elevator_queue *e)
 {
 	struct clook_data *nd = e->elevator_data;
-
+	
 	BUG_ON(!list_empty(&nd->queue));
 	kfree(nd);
 }
 
+// STRUCT:  Specify our 'elevator_clook' functions for the elevator
+/* MADE CHANGES HERE */
 static struct elevator_type elevator_clook = {
 	.ops = {
-		.elevator_merge_req_fn		= clook_merged_requests,
-		.elevator_dispatch_fn		= clook_dispatch,
-		.elevator_add_req_fn		= clook_add_request,
-		.elevator_queue_empty_fn	= clook_queue_empty,
-		.elevator_former_req_fn		= clook_former_request,
-		.elevator_latter_req_fn		= clook_latter_request,
-		.elevator_init_fn		= clook_init_queue,
-		.elevator_exit_fn		= clook_exit_queue,
+		.elevator_merge_req_fn          = clook_merged_requests,
+		.elevator_dispatch_fn           = clook_dispatch,
+		.elevator_add_req_fn            = clook_add_request,
+		.elevator_former_req_fn         = clook_former_request,
+		.elevator_init_fn               = clook_init_queue,
+		.elevator_exit_fn               = clook_exit_queue,
 	},
 	.elevator_name = "clook",
 	.elevator_owner = THIS_MODULE,
 };
 
+// Initialize clook
 static int __init clook_init(void)
 {
-	elv_register(&elevator_clook);
-
-	return 0;
+	return elv_register(&elevator_clook);
 }
 
+// Exit clook
 static void __exit clook_exit(void)
 {
 	elv_unregister(&elevator_clook);
@@ -142,7 +167,7 @@ static void __exit clook_exit(void)
 module_init(clook_init);
 module_exit(clook_exit);
 
-
 MODULE_AUTHOR("Jason Davila");
 MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("CLOOK scheduler");
+MODULE_DESCRIPTION("CLOOK IO Scheduler");
+
