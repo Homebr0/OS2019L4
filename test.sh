6e4a789f17231c9c3576811a39ff5d8ec973b502
Jason Davila <jsndavila1@gmail.com>
2019-11-27 17:06:14 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 LISTLEN=201
-TESTFILE=/test/bashpipingtest
-TESTFILE2=/test/bashtest2
+TESTFILE="test1.txt"
+TESTFILE2="test2.txt"
 
 for i in `seq 1 $LISTLEN`
 do
-	echo "YOUR TEST FACE x$i TIMES" >> $TESTFILE
-	echo "ANOTHER TEST x$i" >> $TESTFILE2
-	echo "AHAHAHAHA x$i TIMES" >> $TESTFILE
+	echo "Test 1x$i TIMES" >> $TESTFILE
+	echo "Test 2x$i TIMES" >> $TESTFILE2
+	echo "Test 3x$i TIMES" >> $TESTFILE
 done
 
 TESTSIZE=$(stat -c%s "$TESTFILE")
